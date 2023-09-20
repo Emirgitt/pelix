cfd5d4114ac79ac2405bfaf5f7e790093e464a2a
Emirgitt <lucasemirsorroza@gmail.com>
2023-09-20 19:07:10 -0300

    vesion 1.1

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function comenzar(){    
+    window.location.replace("peliculas/index.html");
+}
+const comenzar_btn = document.getElementById("btn");
+comenzar_btn.addEventListener("click",comenzar);
